fc7df638b9ba6b5513772bb1c73f23cd6e1f4539
Duex <deux02030@gmail.com>
2017-10-21 12:43:17 -0400

    docs: Add q1
    Technical interview question one python file.

---
+++ q1.py
@@ -0,0 +1,18 @@
+def is_anagram(s1, s2):
+    s1 = sorted(s1)
+    s2 = sorted(s2)
+    return(s1 == s2)
+
+def question1(s,t):
+    for i in range(len(s) - len(t) + 1):
+        if is_anagram(s[i: i + len(t)], t):
+            return True
+    return False
+                   
+
+# test Cases
+
+print question1('university','tn') # False
+print question1('Udacity', 'da') # True
+print question1('programming', 'gram') # True
+    
